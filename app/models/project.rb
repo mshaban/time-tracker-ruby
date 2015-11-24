984ebca48683e2350ede5394b9ecde3a50c025b8
shaban <mohamed19936@gmail.com>
2015-11-24 10:01:56 +0100

    finished domain modelling

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,11 @@ class Project < ActiveRecord::Base
   belongs_to :company
   has_many :works
   has_many :users , :through => :works
+
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :company, presence: true
+  validates :default_rate, numericality: { only_integer: true,
+                                           greater_than: 50 }
+
+  scope :lowdefaultrate, ->{ where("default_rate < 100") }
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,26 @@
 class Work < ActiveRecord::Base
+
   belongs_to :project
   belongs_to :user
+
+  validates :project,
+            :user ,
+            :datetimeperformed, presence: true
+
+  validates :hours, numericality: { only_integer: true,
+                                    greater_than: 0,
+                                    less_than: 8 }
+
+  validate :date_is_in_past
+
+  scope :fullday, -> { where("hours >= 8") }
+  scope :recent, -> { where ("datetimeperformed > '#{Time.now - 7.days}' ") }
+
+
+  def date_is_in_past
+    if datetimeperformed.present? && datetimeperformed > Time.now
+      errors.add(:datetimeperformed, "Can't be in the future")
+    end
+  end
+
 end
