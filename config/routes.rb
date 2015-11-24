bb5afd43335b0a6c3d9e3c5d6cdf0a0712d64ebe
shaban <mohamed19936@gmail.com>
2015-11-24 15:04:57 +0100

    added projects controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+
+  get 'projects/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -15,6 +19,7 @@ Rails.application.routes.draw do
   #   resources :products
 
   resources :companies
+  resources :projects
 
   # Example resource route with options:
   #   resources :products do
